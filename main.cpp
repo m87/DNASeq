697585749d0053ee6fb2a0a8bd137c682be09186
m87 <m87@blackhole@gmail.com>
2014-05-22 09:20:50 +0200

    init4

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string DNA;
+int n,k;
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+    
+
+   
+    return 0;
+}
